2b9a5a9e1b4a7f17649a8b4ae7175ad03d068781
Oleksandr Deundiak <Deundiak@gmail.com>
2017-10-17 16:51:58 +0300

    Added basic char UI

---
+++ VirgilMessenger/ChatViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ChatViewController.swift
+//  VirgilMessenger
+//
+//  Created by Oleksandr Deundiak on 10/17/17.
+//  Copyright © 2017 VirgilSecurity. All rights reserved.
+//
+
+import Foundation
+import Chatto
+import ChattoAdditions
+
+class ChatViewController: BaseChatViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    var chatInputPresenter: BasicChatInputBarPresenter!
+    override func createChatInputView() -> UIView {
+        let chatInputView = ChatInputBar.loadNib()
+        var appearance = ChatInputBarAppearance()
+        appearance.sendButtonAppearance.title = NSLocalizedString("Send", comment: "")
+        appearance.textInputAppearance.placeholderText = NSLocalizedString("Type a message", comment: "")
+        self.chatInputPresenter = BasicChatInputBarPresenter(chatInputBar: chatInputView, chatInputItems: self.createChatInputItems(), chatInputBarAppearance: appearance)
+        chatInputView.maxCharactersCount = 1000
+        return chatInputView
+    }
+    
+    func createChatInputItems() -> [ChatInputItemProtocol] {
+        var items = [ChatInputItemProtocol]()
+        items.append(self.createTextInputItem())
+        return items
+    }
+    
+    private func createTextInputItem() -> TextChatInputItem {
+        let item = TextChatInputItem()
+        item.textInputHandler = { [weak self] text in
+            // Your handling logic
+        }
+        return item
+    }
+}
--- VirgilMessenger/ViewController.swift
+++ VirgilMessenger/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.showChat()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    private func showChat() {
+        self.performSegue(withIdentifier: "showChat", sender: self)
+    }
 }
 
