6e1ed9af6a574e9a091e05ef99aa412f7524bc91
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2017-11-28 12:46:05 +0200

    Useless gesture recognizer removed

--- VirgilMessenger/Profile/ProfileViewController.swift
+++ VirgilMessenger/Profile/ProfileViewController.swift
@@ -12,6 +12,9 @@ import AVFoundation
 
 class ProfileViewController: ViewController {
     
+    private var eggCounter = 0
+    private var player: AVAudioPlayer?
+    
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +31,34 @@ extension ProfileViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        if indexPath.section == 1 {
+        if indexPath.section == 0 {
+            self.eggCounter += 1
+            
+            if self.eggCounter == 20 {
+                self.eggCounter = 0
+                
+                guard let url = Bundle.main.url(forResource: "woody-woodpecker-laugh", withExtension: "mp3") else { return }
+                
+                do {
+                    try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+                    try AVAudioSession.sharedInstance().setActive(true)
+                    
+                    
+                    /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+                    player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+                    
+                    /* iOS 10 and earlier require the following line:
+                     player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+                    
+                    guard let player = player else { return }
+                    
+                    player.play()
+                    
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            }
+        } else if indexPath.section == 1 {
             let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             alert.addAction(UIAlertAction(title: "Logout", style: .destructive) { _ in
                 UserDefaults.standard.set(nil, forKey: "last_username")
