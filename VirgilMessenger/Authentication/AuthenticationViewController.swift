468c93ee31e686f4b8027d99d3113bb69a41fb53
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2017-12-27 16:07:05 +0200

    Center accounts collection when one account.

--- VirgilMessenger/Authentication/AuthenticationViewController.swift
+++ VirgilMessenger/Authentication/AuthenticationViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class AuthenticationViewController: ViewController {
     
+    @IBOutlet weak var collectionViewWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        if CoreDataHelper.sharedInstance.accounts.count == 1 {
+            collectionViewWidthConstraint.constant = 100
+        } else {
+            collectionViewWidthConstraint.constant = 225
+        }
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
         super.viewDidAppear(animated)
     }
