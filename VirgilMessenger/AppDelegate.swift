4a0594d887f184ba442720f2ce992e8a493a53dc
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2018-03-21 20:23:15 +0200

    Handle and log error while saving context

--- VirgilMessenger/AppDelegate.swift
+++ VirgilMessenger/AppDelegate.swift
@@ -108,7 +108,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                Log.error("save context failed: \(nserror.localizedDescription)")
             }
         }
     }
