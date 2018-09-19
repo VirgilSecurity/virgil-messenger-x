fee335d1efb5f905f50a839e74f04d6885dafbae
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2018-09-19 18:44:19 +0300

    Fix status bar style swift 4.2 warning

--- VirgilMessenger/AppDelegate.swift
+++ VirgilMessenger/AppDelegate.swift
@@ -21,7 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        UIApplication.shared.statusBarStyle = .lightContent
         UIApplication.shared.delegate?.window??.rootViewController = UIStoryboard(name: StartViewController.name, bundle: Bundle.main).instantiateInitialViewController()!
 
         if UserDefaults.standard.string(forKey: "first_launch")?.isEmpty ?? true {
