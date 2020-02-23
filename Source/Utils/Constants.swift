4a5619c2b2568b6cd2b30b8f0872a35fb4285dbd
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2020-02-23 20:57:16 +0200

    Switch to stg

--- Source/Utils/Constants.swift
+++ Source/Utils/Constants.swift
@@ -12,7 +12,7 @@ import XMPPFrameworkSwift
 enum Constants {
     static let appId = Bundle.main.bundleIdentifier!
     static let pushesNode: String = "node"
-    static let appGroup: String = "group.virgil.notification"
+    static let appGroup: String = "group.virgil.notifications"
 }
 
 enum URLConstants {
@@ -24,7 +24,7 @@ enum URLConstants {
         case prod = ""
     }
 
-    static let envPrefix: EnvPrefix = .prod
+    static let envPrefix: EnvPrefix = .stg
 
     // Ejabberd
     static let ejabberdHost: String = "xmpp\(URLConstants.envPrefix.rawValue).virgilsecurity.com"
