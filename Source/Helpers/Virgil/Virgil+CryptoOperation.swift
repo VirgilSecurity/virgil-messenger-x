edf464bdeb1e17b658ebaa002d283d400a5dcfbe
SergeySeroshtan <sergey.seroshtan@gmail.com>
2020-04-02 17:36:12 +0300

    Add file Virgil+CryptoOpertaions.swift

---
+++ Source/Helpers/Virgil/Virgil+CryptoOperation.swift
@@ -0,0 +1,9 @@
+//
+//  Virgil+CryptoOperation.swift
+//  VirgilMessenger
+//
+//  Created by Sergey Seroshtan on 02.04.2020.
+//  Copyright © 2020 VirgilSecurity. All rights reserved.
+//
+
+import Foundation
