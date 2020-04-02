e783f9fe56d091e7df714481b7aa8076505f0175
SergeySeroshtan <sergey.seroshtan@gmail.com>
2020-04-02 17:56:16 +0300

    Add new file Storage+MessageUI
    (cherry picked from commit 5ee670674250e7954e7599447655f12ac4c8f6f7)
    
    # Conflicts:
    #	VirgilMessenger.xcodeproj/project.pbxproj

---
+++ Source/Storage/Storage+MessageUI.swift
@@ -0,0 +1,9 @@
+//
+//  Storage+MessageUI.swift
+//  VirgilMessenger
+//
+//  Created by Sergey Seroshtan on 02.04.2020.
+//  Copyright © 2020 VirgilSecurity. All rights reserved.
+//
+
+import Foundation
