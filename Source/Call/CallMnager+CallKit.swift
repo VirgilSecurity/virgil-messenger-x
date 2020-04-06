ca00dfe020b9f2e231859045f6e31e94dfd668f8
SergeySeroshtan <sergey.seroshtan@gmail.com>
2020-04-06 11:22:36 +0300

    Add file "CallManager+CallKit.swift"

---
+++ Source/Call/CallMnager+CallKit.swift
@@ -0,0 +1,9 @@
+//
+//  CallMnager+CallKit.swift
+//  VirgilMessenger
+//
+//  Created by Sergey Seroshtan on 06.04.2020.
+//  Copyright © 2020 VirgilSecurity. All rights reserved.
+//
+
+import Foundation
