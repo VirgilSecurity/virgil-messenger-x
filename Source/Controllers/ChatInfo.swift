db56f97c8af7a046979ad1a4557c49ccd8322045
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2020-05-13 18:27:07 +0300

    Added top part of chat info

--- Source/Controllers/ChatInfo.swift
+++ Source/Controllers/ChatInfo.swift
@@ -6,10 +6,28 @@
 //  Copyright © 2020 VirgilSecurity. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class ChatInfoViewController: ViewController {
+    @IBOutlet weak var avatarLabel: UILabel!
+    @IBOutlet weak var avatarView: GradientView!
+    @IBOutlet weak var usernameLabel: UILabel!
+
     var channel: Storage.Channel!
 
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.avatarLabel.text = self.channel.letter
+        self.avatarView.draw(with: self.channel.colors)
+        self.usernameLabel.text = self.channel.name
+    }
+
+    @IBAction func startCallTapped(_ sender: Any) {
+        CallManager.shared.startOutgoingCall(to: self.channel.name)
+    }
+
+    @IBAction func messageTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
