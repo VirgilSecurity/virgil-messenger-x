cd31b2f87f9395cc32e385a9921b1b0270bbb714
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2020-05-25 12:44:31 +0300

    Set chatMaxCharacters count to 1000

--- Source/Utils/Constants/Constants.swift
+++ Source/Utils/Constants/Constants.swift
@@ -61,7 +61,7 @@ enum URLConstants {
 enum ChatConstants {
     static let limitLength = 32
     static let characterSet = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-()/='+:?!%&*<>;{}#_")
-    static let chatMaxCharectersCount: UInt = 200
+    static let chatMaxCharactersCount: UInt = 1000
     static let chatPageSize = 20
 }
 
